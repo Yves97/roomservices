4f301c8c113b31b0b9c6d228c70f1e81cef1fcd5
YVES KOUADIO <yveskouadio@192.168.10.101>
2022-06-23 14:18:54 +0000

    add heroku database postgres credentials

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -1,12 +1,12 @@
 import {TypeOrmModuleOptions} from '@nestjs/typeorm'
 
 export const typeOrmConfig : TypeOrmModuleOptions = {
-    type : 'mysql',
-    host : 'localhost',
-    port : 3306,
-    username : 'root',
-    password : 'amaniyves',
-    database : 'roomservices',
+    type : 'postgres',
+    host : 'ec2-3-224-8-189.compute-1.amazonaws.com',
+    port : 5432,
+    username : 'tdzwweibccnjmr',
+    password : '6b342f95db4ebc5eced5b50e88208af3e20f105d860ac1c6c1a5a5e667f9e544',
+    database : 'd3qdmlqtu1viu5',
     entities : [__dirname + '/../**/*.entity{.ts,.js}'],
     synchronize : true
 }
