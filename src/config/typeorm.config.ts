d16252a6f31d3d5f9985634a19acb7d179c73a0d
YVES KOUADIO <yveskouadio@192.168.10.101>
2022-06-23 14:41:36 +0000

    deploy() add no ssl rejectAuth

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -7,6 +7,9 @@ export const typeOrmConfig : TypeOrmModuleOptions = {
     username : 'tdzwweibccnjmr',
     password : '6b342f95db4ebc5eced5b50e88208af3e20f105d860ac1c6c1a5a5e667f9e544',
     database : 'd3qdmlqtu1viu5',
+    ssl: {
+        rejectUnauthorized: false,
+    },
     entities : [__dirname + '/../**/*.entity{.ts,.js}'],
     synchronize : true
 }
